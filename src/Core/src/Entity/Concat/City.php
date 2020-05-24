58748e21d155cc10ea1835c3a226f417c24ed8f8
Jonik <jonikdoit@gmail.com>
2020-05-24 17:38:33 +0300

    город в concat

--- src/Core/src/Entity/Concat/City.php
+++ src/Core/src/Entity/Concat/City.php
@@ -5,6 +5,7 @@ namespace Core\Entity\Concat;
 
 
 use Core\Entity\Country;
+use Core\Entity\Region;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -17,5 +18,46 @@ class City extends \Core\Entity\City
      * @ORM\ManyToOne(targetEntity="Core\Entity\Country")
      * @ORM\JoinColumn(name="country_id", referencedColumnName="id")
      */
-    protected Country $region;
+    protected Country $country;
+    /**
+     * @ORM\ManyToOne(targetEntity="Core\Entity\Region")
+     * @ORM\JoinColumn(name="region_id", referencedColumnName="id")
+     */
+    protected Region $region;
+
+    /**
+     * @return Country
+     */
+    public function getCountry(): Country
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param Country $country
+     * @return City
+     */
+    public function setCountry(Country $country): City
+    {
+        $this->country = $country;
+        return $this;
+    }
+
+    /**
+     * @return Region
+     */
+    public function getRegion(): Region
+    {
+        return $this->region;
+    }
+
+    /**
+     * @param Region $region
+     * @return City
+     */
+    public function setRegion(Region $region): City
+    {
+        $this->region = $region;
+        return $this;
+    }
 }
